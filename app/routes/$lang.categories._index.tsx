cc4698a20c42d72c71c00251885f85de9ba05ec6
Kevin Shreve <kshreve@gmail.com>
2023-12-30 14:36:29 -0500

    add links

--- app/routes/$lang.categories._index.tsx
+++ app/routes/$lang.categories._index.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useParams } from "@remix-run/react"
+import { Link, useLoaderData, useParams } from "@remix-run/react"
 import { json, LoaderFunction } from "@remix-run/node"
 import { Category, getCategories } from "~/sanity/queries"
 import { client } from "~/sanity/client"
@@ -6,6 +6,7 @@ import { SupportedLanguages } from "~/i18n"
 import isLangSupportedLang from "~/sanity/queries/isLangSupportedLang"
 import { Layout } from "~/components/Layout"
 import { Typography } from "~/components/Typography"
+import { zeroWidthTrim } from "~/lib/zeroWidthTrim"
 
 interface IndexLoaderData {
   categories: Category[]
@@ -31,14 +32,22 @@ export default function CategoriesRoute() {
       useMargins
     >
       <div>
-        {categories?.map((category) => (
-          <div key={category.name[lang]}>
-            <Typography.H3>{category.name[lang]}</Typography.H3>
-            <Typography.TextMuted key={category.description[lang]}>
-              {category.description[lang]}
-            </Typography.TextMuted>
-          </div>
-        ))}
+        {categories?.map((category) => {
+          const linkTo = zeroWidthTrim(
+            `/${lang}/categories/${category.name[lang]}`
+          )
+
+          return (
+            <div key={category.name[lang]}>
+              <Link prefetch="intent" to={linkTo}>
+                <Typography.H3>{category.name[lang]}</Typography.H3>
+                <Typography.TextMuted key={category.description[lang]}>
+                  {category.description[lang]}
+                </Typography.TextMuted>
+              </Link>
+            </div>
+          )
+        })}
       </div>
     </Layout>
   )
