b13e8c1c92cbea4419c9ad3be76e7639555eb1ae
Tony Mamo <tmamo13991@gmail.com>
2024-01-02 15:09:42 -0500

    testing out something with toasts

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -14,6 +14,7 @@ import {
   TooltipTrigger,
 } from "~/components/ui/tooltip"
 import { useTranslate } from "~/lib/useTranslate"
+import { toast } from "sonner"
 
 export function ThemeToggle() {
   const [switchOn] = useSound(onSound)
@@ -41,6 +42,9 @@ export function ThemeToggle() {
               aria-label={translate("toggleTheme")}
               onClick={() => {
                 isDarkMode ? switchOn() : switchOff()
+                toast.success(
+                  "Theme changed to " + (isDarkMode ? "dark" : "light") + "."
+                )
               }}
             >
               {isDarkMode ? <Sun /> : <Moon />}
