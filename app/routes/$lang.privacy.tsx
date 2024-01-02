d2c5e6575799ac8211aef7bf28cb9e70c4552520
Kevin Shreve <kshreve@gmail.com>
2024-01-02 13:27:57 -0500

    setup static routes

---
+++ app/routes/$lang.privacy.tsx
@@ -0,0 +1,23 @@
+import { Layout } from "~/components/Layout"
+import { Typography } from "~/components/Typography"
+import isLangSupportedLang from "~/sanity/queries/isLangSupportedLang"
+import { json, LoaderFunction } from "@remix-run/node"
+
+export const loader: LoaderFunction = async ({ params }) => {
+  isLangSupportedLang(params.lang)
+
+  return json({}, { status: 200 })
+}
+
+const Privacy = () => {
+  return (
+    <Layout useMargins>
+      <Typography.H1>Privacy Policy</Typography.H1>
+      <Typography.TextSmall>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      </Typography.TextSmall>
+    </Layout>
+  )
+}
+
+export default Privacy
