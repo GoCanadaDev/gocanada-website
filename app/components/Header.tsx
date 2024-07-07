9129a851ea94c236e6d01b5fc801b552f5de3252
Tony Mamo <tmamo13991@gmail.com>
2024-07-07 08:09:38 -0400

    comment out header and footer in Layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -21,7 +21,7 @@ export function Header({ translationUrl }: { translationUrl?: string }) {
       >
         Skip to main content
       </a>
-      {/* <div className=" border-b-4 border-slate-100 transition-colors duration-1000 ease-in-out dark:border-slate-800">
+      <div className=" border-b-4 border-slate-100 transition-colors duration-1000 ease-in-out dark:border-slate-800">
         <div className="container mx-auto flex items-center justify-between p-2 lg:px-12">
           <Logo />
           <div className="flex items-center gap-4">
@@ -66,7 +66,7 @@ export function Header({ translationUrl }: { translationUrl?: string }) {
             Subscribe
           </div>
         </div>
-      </div> */}
+      </div>
     </header>
   )
 }
