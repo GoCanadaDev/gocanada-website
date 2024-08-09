2714507099db3f186b598aaa677b7274288810f4
Tony Mamo <tmamo13991@gmail.com>
2024-08-09 09:13:22 -0400

    fix crash of studio when image ref isnt there yet

--- app/components/portable/GalleryImages/GridImages.tsx
+++ app/components/portable/GalleryImages/GridImages.tsx
@@ -4,17 +4,20 @@ import { GalleryImagesProps } from "~/components/portable/GalleryImages/index"
 import { SingleImage } from "~/components/portable/GalleryImages/SingleImage"
 
 export const GridImages = ({ value }: GalleryImagesProps) => {
-  if (!value) return null
+  if (!value || !value.images || value.images.length === 0) return null
 
   return (
     <div className="grid grid-cols-2 gap-4 [&>figure]:m-0">
-      {value.images.map((image) => (
-        <SingleImage
-          value={image}
-          key={image.asset._ref}
-          className="aspect-square object-cover"
-        />
-      ))}
+      {value?.images?.map((image) => {
+        if (!image || !image.asset?._ref) return null
+        return (
+          <SingleImage
+            value={image}
+            key={image.asset._ref}
+            className="aspect-square object-cover"
+          />
+        )
+      })}
     </div>
   )
 }
