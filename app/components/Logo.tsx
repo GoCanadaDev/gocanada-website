6771865fa06d049ff25a333b1d9f6fdc9f858baa
Tony Mamo <tmamo13991@gmail.com>
2024-08-14 09:55:13 -0400

    dark mode logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,14 +1,23 @@
 import { Link } from "@remix-run/react"
-import GoCanadaLogo from "../../public/images/logotype-colour.png"
+import GoCanadaLogoColour from "../../public/images/logotype-colour.png"
+import GoCanadaLogoReverse from "../../public/images/logotype-colour-reverse.png"
+import { useRootLoaderData } from "~/lib/useRootLoaderData"
 
 export function Logo() {
+  const { themePreference } = useRootLoaderData()
   return (
     <Link
       to="/"
       className="rounded-md hover:opacity-90 focus:opacity-90 focus-visible:outline-none active:bg-transparent "
     >
       <span className="sr-only">GoCanada</span>
-      <img src={GoCanadaLogo} alt="GoCanada Logo" className="w-32 md:w-48" />
+      <img
+        src={
+          themePreference === "dark" ? GoCanadaLogoReverse : GoCanadaLogoColour
+        }
+        alt="GoCanada Logo"
+        className="w-32 md:w-48"
+      />
     </Link>
   )
 }
