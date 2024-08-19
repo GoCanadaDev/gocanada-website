6da2b9bad90f03bf84636d51060892406d953497
Tony Mamo <tmamo13991@gmail.com>
2024-08-18 21:03:35 -0400

    robots.txt file

---
+++ app/routes/[robots.txt].tsx
@@ -0,0 +1,14 @@
+export const loader = () => {
+  const robotText = `
+        User-agent: Googlebot
+        Disallow: /nogooglebot/
+        User-agent: *
+        Allow: /`
+
+  return new Response(robotText, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  })
+}
