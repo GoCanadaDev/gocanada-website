38b44ac22a8d179294526ff6af07ca9843ddb9c9
Tony Mamo <tmamo13991@gmail.com>
2024-08-30 20:38:46 -0400

    fix card on really large viewports

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -24,7 +24,7 @@ export default function Card({
   }
 
   return (
-    <article className="group max-w-screen-md">
+    <article className="group">
       {post.mainImage && post.mainImage?.id ? (
         <div
           className={cn("", {
@@ -40,12 +40,12 @@ export default function Card({
                 mode="cover"
                 id={post.mainImage.id}
                 alt=""
-                // if hideImage, we need a bigger size for the image a the sm breakpoint
-                width={isLarge || hideImage ? 768 : 576}
-                height={isLarge || hideImage ? 512 : 384}
+                // if hideImage, we need a bigger size for the image at the sm breakpoint
+                width={isLarge || hideImage ? 1024 : 576}
+                height={isLarge || hideImage ? 640 : 384}
                 preview={post.mainImage.preview ?? ""}
                 loading="eager"
-                className="transition-transform hover:scale-[1.05]"
+                className="h-full object-cover transition-transform hover:scale-[1.05]"
                 aria-label={`Read more: ${post.title[post.language]}`}
                 hotspot={post.mainImage.hotspot}
                 crop={post.mainImage.crop}
