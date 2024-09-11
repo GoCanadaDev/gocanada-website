ce3938d839feb0b03b07c890a07b025d30cf54ff
Tony Mamo <tmamo13991@gmail.com>
2024-09-11 17:06:38 -0400

    og image full bg with logo only

--- app/lib/og.server.tsx
+++ app/lib/og.server.tsx
@@ -8,14 +8,14 @@ import { OG_IMAGE_HEIGHT, OG_IMAGE_WIDTH } from "~/routes/resource.og"
 import { dataset, projectId } from "~/sanity/projectDetails"
 
 // Load the font from the "public" directory
-const fontSerif = (baseUrl: string) =>
-  fetch(new URL(`${baseUrl}/fonts/Rasa-Regular.ttf`)).then((res) =>
-    res.arrayBuffer()
-  )
-const fontSans = (baseUrl: string) =>
-  fetch(new URL(`${baseUrl}/fonts/PTSans-Regular.ttf`)).then((res) =>
-    res.arrayBuffer()
-  )
+// const fontSerif = (baseUrl: string) =>
+//   fetch(new URL(`${baseUrl}/fonts/Rasa-Regular.ttf`)).then((res) =>
+//     res.arrayBuffer()
+//   )
+// const fontSans = (baseUrl: string) =>
+//   fetch(new URL(`${baseUrl}/fonts/PTSans-Regular.ttf`)).then((res) =>
+//     res.arrayBuffer()
+//   )
 
 export async function generatePngFromDocument(
   doc: SanityDocument,
@@ -24,22 +24,22 @@ export async function generatePngFromDocument(
   const { title, mainImage, author } = doc
 
   // Prepare font data and settings for Satori
-  const fontSerifData = await fontSerif(origin)
-  const fontSansData = await fontSans(origin)
+  // const fontSerifData = await fontSerif(origin)
+  // const fontSansData = await fontSans(origin)
   const options: SatoriOptions = {
     width: OG_IMAGE_WIDTH,
     height: OG_IMAGE_HEIGHT,
     fonts: [
-      {
-        name: "Rasa",
-        data: fontSerifData,
-        style: "normal",
-      },
-      {
-        name: "PT Sans",
-        data: fontSansData,
-        style: "normal",
-      },
+      // {
+      //   name: "Rasa",
+      //   data: fontSerifData,
+      //   style: "normal",
+      // },
+      // {
+      //   name: "PT Sans",
+      //   data: fontSansData,
+      //   style: "normal",
+      // },
     ],
   }
 
@@ -47,55 +47,29 @@ export async function generatePngFromDocument(
   const svg = await satori(
     <div
       style={{
-        width: 1200,
-        height: 630,
+        width: OG_IMAGE_WIDTH,
+        height: OG_IMAGE_HEIGHT,
         background: "#bf2327",
         color: "white",
-        fontFamily: "Rasa",
+        // fontFamily: "Rasa",
         letterSpacing: "-0.05em",
         display: "flex",
         lineHeight: 1,
+        position: "relative",
       }}
     >
-      <div
-        style={{
-          width: 600,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          padding: (OG_IMAGE_HEIGHT - 500) / 2,
-          gap: 25,
-        }}
-      >
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <div style={{ fontSize: 50, marginBottom: 25 }}>{title.en}</div>
-          <p
-            style={{
-              fontSize: 20,
-              fontFamily: "'PT Sans', sans-serif",
-              textTransform: "uppercase",
-              letterSpacing: "0.3em",
-            }}
-          >
-            By {author.name}
-          </p>
-        </div>
-        <img
-          src="https://gocanada.com/images/logotype-white.png"
-          width={300}
-          height={59}
-        />
-      </div>
       {mainImage?.asset?._ref ? (
         <div
           style={{
-            width: 600,
-            height: 500,
+            width: OG_IMAGE_WIDTH,
+            height: OG_IMAGE_HEIGHT,
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             alignSelf: "center",
             overflow: "hidden",
+            position: "absolute",
+            inset: 0,
           }}
         >
           <img
@@ -103,16 +77,35 @@ export async function generatePngFromDocument(
             src={urlBuilder({ projectId, dataset })
               // @ts-ignore
               .image(mainImage.asset._ref)
-              .height(800)
-              .width(800)
+              .height(OG_IMAGE_HEIGHT)
+              .width(OG_IMAGE_WIDTH)
               .fit("max")
               .auto("format")
               .url()}
-            width="600"
-            height="500"
+            width={OG_IMAGE_WIDTH}
+            height={OG_IMAGE_HEIGHT}
           />
         </div>
       ) : null}
+      <div
+        style={{
+          width: 600,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          // padding: (OG_IMAGE_HEIGHT - 500) / 2,
+          // gap: 25,
+          position: "absolute",
+          bottom: 50,
+          left: 50,
+        }}
+      >
+        <img
+          src="https://gocanada.com/images/logotype-white.png"
+          width={300}
+          height={57.76}
+        />
+      </div>
     </div>,
     options
   )
