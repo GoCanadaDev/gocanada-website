8f4fe027c979ebef311010fafd3c77a986fd816f
Tony Mamo <tmamo13991@gmail.com>
2024-09-11 14:06:32 -0400

    more og tags

--- app/routes/$lang._index.tsx
+++ app/routes/$lang._index.tsx
@@ -37,10 +37,30 @@ export const meta: MetaFunction<typeof loader> = ({
     },
     { property: "twitter:card", content: "summary_large_image" },
     { property: "twitter:title", content: title },
+    { property: "twitter:site", content: "@gocanada" },
+    {
+      property: "twitter:description",
+      content: data.siteConfig.siteDescription,
+    },
+    { property: "twitter:image", content: "/images/og-main.png" },
+    {
+      property: "twitter:image:alt",
+      content: "White logo for Go Canada on a red background",
+    },
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://gocanada.com/en" },
     { property: "og:title", content: title },
+    { property: "og:site_name", content: "Go Canada" },
+    { property: "og:locale", content: "en_CA" },
+    { property: "og:description", content: data.siteConfig.siteDescription },
     { property: "og:image:width", content: String(OG_IMAGE_WIDTH) },
     { property: "og:image:height", content: String(OG_IMAGE_HEIGHT) },
-    { property: "og:image", content: "/public/images/og-main.png" },
+    { property: "og:image", content: "/images/og-main.png" },
+    { property: "og:image:type", content: "image/png" },
+    {
+      property: "og:image:alt",
+      content: "White logo for Go Canada on a red background",
+    },
   ]
 }
 
