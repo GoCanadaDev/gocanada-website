00bc32a7e40fde846dd3659131ba8e2959cbbe16
Tony Mamo <tmamo13991@gmail.com>
2024-09-27 11:35:26 -0400

    disable auto theme preference

--- app/lib/getBodyClassNames.ts
+++ app/lib/getBodyClassNames.ts
@@ -1,9 +1,9 @@
 export function getBodyClassNames(themePreference?: string): string {
   // Use browser default if cookie is not set
-  const isDarkMode =
-    !themePreference && typeof document !== "undefined"
-      ? window.matchMedia("(prefers-color-scheme: dark)").matches
-      : themePreference === `dark`
+  const isDarkMode = themePreference === `dark`
+  // !themePreference && typeof document !== "undefined"
+  //   ? window.matchMedia("(prefers-color-scheme: dark)").matches
+  //   : themePreference === `dark`
   return [
     `transition-colors duration-500 ease-in-out min-h-screen font-sans-serif min-h-screen grid grid-rows-[auto_1fr_auto] relative`,
     isDarkMode ? `dark bg-zinc-900 text-zinc-300` : `bg-white text-zinc-900`,
