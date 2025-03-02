3e2e6529b0d27520b7108554bcfe43cf1aba3f7f
Tony Mamo <tmamo13991@gmail.com>
2025-03-02 14:09:01 -0500

    preview mode button styling

--- app/components/Preview.tsx
+++ app/components/Preview.tsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "@remix-run/react"
+import { LogOut } from "lucide-react"
 
 export function Preview({ children }: { children: React.ReactNode }) {
   const { isDraftMode } = useLoaderData<{ isDraftMode: boolean }>()
@@ -6,25 +7,12 @@ export function Preview({ children }: { children: React.ReactNode }) {
   return (
     <>
       {isDraftMode && (
-        <div
-          style={{
-            backgroundColor: "#f00",
-            color: "#fff",
-            padding: "10px",
-            position: "fixed",
-            bottom: 0,
-            right: 0,
-            zIndex: 9999,
-          }}
+        <a
+          href="/api/disable-draft"
+          className="fixed right-0 top-0 z-50 flex gap-2 rounded-bl bg-brand p-3 text-sm text-white transition-all duration-200 hover:gap-4 hover:bg-brandHover"
         >
-          Preview Mode
-          <a
-            href="/api/disable-draft"
-            style={{ marginLeft: "10px", color: "#fff" }}
-          >
-            Exit Preview
-          </a>
-        </div>
+          Exit Preview Mode <LogOut className="inline-block size-4" />
+        </a>
       )}
       {children}
     </>
