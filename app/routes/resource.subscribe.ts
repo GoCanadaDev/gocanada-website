cbc865eb28b390a11f300eb35b8b367ef11d606a
Tony Mamo <tmamo13991@gmail.com>
2025-08-07 14:07:43 -0400

    send email after subscription

--- app/routes/resource.subscribe.ts
+++ app/routes/resource.subscribe.ts
@@ -1,14 +1,14 @@
 import type { ActionFunction, LoaderFunction } from "@remix-run/node"
 
-//import sendgridMail from "@sendgrid/mail"
 import { ClientRequest } from "@sendgrid/client/src/request"
 import getObjectFromFormData from "~/lib/getObjectFromFormData"
 import { langPreferenceCookie } from "~/cookies.server"
 import { redirect } from "@remix-run/node"
 import sendgrid from "@sendgrid/client"
+import sendgridMail from "@sendgrid/mail"
 
 sendgrid.setApiKey(String(process.env.SENDGRID_API_KEY))
-// sendgridMail.setApiKey(String(process.env.SENDGRID_API_KEY))
+sendgridMail.setApiKey(String(process.env.SENDGRID_API_KEY))
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
@@ -37,30 +37,30 @@ export const action: ActionFunction = async ({ request }) => {
   }
   await sendgrid
     .request(req as ClientRequest)
-    .then(([response, body]) => {
+    .then(async ([response, body]) => {
       console.log({ status: response.statusCode })
       console.log({ body })
+      // after successful signup, send welcome email
+      const msg = {
+        to: formInput.email,
+        from: {
+          email: "news@gocanada.com",
+          name: "Go Canada",
+        },
+        templateId: "d-6ae7e3266ecb405fa8d8bb6d42056d10",
+        dynamicTemplateData: {
+          email: formInput.email,
+          pathname: formInput.pathname ?? "",
+          pageLocation: formInput.pageLocation ?? "",
+        },
+      }
+      await sendgridMail.send(msg)
     })
     .catch((error) => {
       console.error({
         error,
       })
     })
-
-  // const msg = {
-  //   to: formInput.email,
-  //   from: {
-  //     email: "news@gocanada.com",
-  //     name: "Go Canada",
-  //   },
-  //   templateId: "d-6ae7e3266ecb405fa8d8bb6d42056d10",
-  //   dynamicTemplateData: {
-  //     email: formInput.email,
-  //     pathname: formInput.pathname ?? "",
-  //     pageLocation: formInput.pageLocation ?? "",
-  //   },
-  // }
-  // await sendgridMail.send(msg)
 
   return null
 }
