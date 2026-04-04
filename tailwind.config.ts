a4f8a0ff87b06d9bce523d39e92bcba6dd1ee50e
Tony Mamo <tmamo13991@gmail.com>
2026-04-04 11:23:39 -0400

    buttons page and svg logos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ const config = {
       colors: {
         brand: "#bf2327",
         brandHover: "#d64945",
+        brandDark: "#a00007",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
