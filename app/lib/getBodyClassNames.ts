ac4e92b6244ef30322ebc0dc5b493959e0da1dec
Tony Mamo <tmamo13991@gmail.com>
2026-04-04 13:16:06 -0400

    remove flex from body classnames as a test

--- app/lib/getBodyClassNames.ts
+++ app/lib/getBodyClassNames.ts
@@ -5,7 +5,7 @@ export function getBodyClassNames(themePreference?: string): string {
   //   ? window.matchMedia("(prefers-color-scheme: dark)").matches
   //   : themePreference === `dark`
   return [
-    `flex min-h-screen flex-col font-sans-serif transition-colors duration-500 ease-in-out`,
+    `font-sans-serif transition-colors duration-500 ease-in-out`,
     isDarkMode ? `dark bg-zinc-900 text-zinc-300` : `bg-white text-zinc-900`,
   ]
     .join(" ")
